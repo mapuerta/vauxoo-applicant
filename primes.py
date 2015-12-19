7157451967620541fb49f0ccbdea1e38e07c8f9f
User <sanchezalgoritmo@gmail.com>
2015-12-19 17:17:44 -0430

    quarter of the evaluation method py1 class Primeclass is_prime

--- primes.py
+++ primes.py
@@ -10,7 +10,15 @@ class PrimeClass(object):
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Funcion para calcular los numeros primos
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        if not isinstance(num_int, int):
+            return False
+        acum = 0
+        # calculo si el numero es primo
+        for num in range(1, num_int+1):
+            if num_int % num == 0:
+                acum += 1
+        if acum == 2:
+            return True
+        return False  # retorno false si no es primo
