e37dc2230656cb01bf9002796e44e0811789952c
User <sanchezalgoritmo@gmail.com>
2015-12-19 17:54:33 -0430

    I made ​​py2 exercise activity , cleaned the code to comply with the standard PEP8 , file calculator.py

--- calculator.py
+++ calculator.py
@@ -10,7 +10,12 @@ class CalculatorClass(object):
 
     def sum(self, num_list):
         """
-        Your method documentation here
+        method to add numbers from a list
         """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+        # assessed if a list
+        if not isinstance(num_list, list):
+            return "it is not a list"
+        num_sum = 0
+        for num in num_list:
+            num_sum += num
+        return num_sum  # return the sum of the items in the list
